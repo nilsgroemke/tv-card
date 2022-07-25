53680c41ff6609d62d0390e078c5c463600581bb
nilsgroemke <56953421+nilsgroemke@users.noreply.github.com>
2022-07-25 19:00:06 +0200

    Update tv-card.js

--- tv-card.js
+++ tv-card.js
@@ -4,25 +4,25 @@ const LitElement = Object.getPrototypeOf(
 const html = LitElement.prototype.html;
 
 const keys = {
-    "power": {"key": "KEY_POWER", "icon": "mdi:power"},
-    "volume_up": {"key": "KEY_VOLUP", "icon": "mdi:volume-plus"},
-    "volume_down": {"key": "KEY_VOLDOWN", "icon": "mdi:volume-minus"},
-    "volume_mute": {"key": "KEY_MUTE", "icon": "mdi:volume-mute"},
-    "return": {"key": "KEY_RETURN", "icon": "mdi:arrow-left"},
-    "source": {"key": "KEY_SOURCE", "icon": "mdi:video-input-hdmi"},
-    "info": {"key": "KEY_INFO", "icon": "mdi:television-guide"},
-    "home": {"key": "KEY_HOME", "icon": "mdi:home"},
-    "channel_up": {"key": "KEY_CHUP", "icon": "mdi:arrow-up"},
-    "channel_down": {"key": "KEY_CHDOWN", "icon": "mdi:arrow-down"},
-    "up": {"key": "KEY_UP", "icon": "mdi:chevron-up"},
-    "left": {"key": "KEY_LEFT", "icon": "mdi:chevron-left"},
-    "enter": {"key": "KEY_ENTER", "icon": "mdi:checkbox-blank-circle"},
-    "right": {"key": "KEY_RIGHT", "icon": "mdi:chevron-right"},
-    "down": {"key": "KEY_DOWN", "icon": "mdi:chevron-down"},
-    "rewind": {"key": "KEY_REWIND", "icon": "mdi:rewind"},
-    "play": {"key": "KEY_PLAY", "icon": "mdi:play"},
-    "pause": {"key": "KEY_PAUSE", "icon": "mdi:pause"},
-    "fast_forward": {"key": "KEY_FF", "icon": "mdi:fast-forward"},
+    "power": {"key": "POWER", "icon": "mdi:power"},
+    "volume_up": {"key": "VOLUP", "icon": "mdi:volume-plus"},
+    "volume_down": {"key": "VOLDOWN", "icon": "mdi:volume-minus"},
+    "volume_mute": {"key": "MUTE", "icon": "mdi:volume-mute"},
+    "return": {"key": "RETURN", "icon": "mdi:arrow-left"},
+    "source": {"key": "SOURCE", "icon": "mdi:video-input-hdmi"},
+    "info": {"key": "INFO", "icon": "mdi:television-guide"},
+    "home": {"key": "HOME", "icon": "mdi:home"},
+    "channel_up": {"key": "CHUP", "icon": "mdi:arrow-up"},
+    "channel_down": {"key": "CHDOWN", "icon": "mdi:arrow-down"},
+    "up": {"key": "UP", "icon": "mdi:chevron-up"},
+    "left": {"key": "LEFT", "icon": "mdi:chevron-left"},
+    "enter": {"key": "ENTER", "icon": "mdi:checkbox-blank-circle"},
+    "right": {"key": "RIGHT", "icon": "mdi:chevron-right"},
+    "down": {"key": "DOWN", "icon": "mdi:chevron-down"},
+    "rewind": {"key": "REWIND", "icon": "mdi:rewind"},
+    "play": {"key": "PLAY", "icon": "mdi:play"},
+    "pause": {"key": "PAUSE", "icon": "mdi:pause"},
+    "fast_forward": {"key": "FF", "icon": "mdi:fast-forward"},
 };
 
 const sources = {
@@ -169,7 +169,7 @@ class TVCardServices extends LitElement {
     onClick(event) {
         event.stopImmediatePropagation();
         let click_action = () => {
-            this.sendKey("KEY_ENTER");
+            this.sendKey("ENTER");
             if (this._config.enable_button_feedback === undefined || this._config.enable_button_feedback) fireEvent(window, "haptic", "light");
         };
         if (this._config.enable_double_click) {
@@ -187,14 +187,14 @@ class TVCardServices extends LitElement {
         clearTimeout(this.timer);
         this.timer = null;
 
-        this.sendKey(this._config.double_click_keycode ? this._config.double_click_keycode : "KEY_RETURN");
+        this.sendKey(this._config.double_click_keycode ? this._config.double_click_keycode : "RETURN");
         if (this._config.enable_button_feedback === undefined || this._config.enable_button_feedback) fireEvent(window, "haptic", "success");
     }
 
     onTouchStart(event) {
         event.stopImmediatePropagation();
 
-        this.holdaction = "KEY_ENTER";
+        this.holdaction = "ENTER";
         this.holdtimer = setTimeout(() => {
             //hold
             this.holdinterval = setInterval(() => {
@@ -230,12 +230,12 @@ class TVCardServices extends LitElement {
 
         if (Math.abs(diffX) > Math.abs(diffY)) {
             // sliding horizontally
-            let key = diffX > 0 ? "KEY_LEFT" : "KEY_RIGHT";
+            let key = diffX > 0 ? "LEFT" : "RIGHT";
             this.holdaction = key;
             this.sendKey(key);
         } else {
             // sliding vertically
-            let key = diffY > 0 ? "KEY_UP" : "KEY_DOWN";
+            let key = diffY > 0 ? "UP" : "DOWN";
             this.holdaction = key;
             this.sendKey(key);
         }
